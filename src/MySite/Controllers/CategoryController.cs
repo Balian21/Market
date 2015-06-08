944a666b1aae5a1ba531db505a73a0201c1818b8
Balian21 <alexpv212121@gmail.com>
2015-06-08 19:00:36 +0300

    Add Index Action, HomeController and Categories

---
+++ src/MySite/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using FluentNHibernate;
+using Model;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //
+        // GET: /Category/
+
+        public ActionResult Categories()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var categories = session.Query<Category>()
+                                    .Where(p => p.Parent != null)
+                                    .ToList();
+                return View(categories);
+            }
+        }
+    }
+}
