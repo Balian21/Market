19d930aef7a8280cdd7e173fe093190a2ddcd418
Balian21 <alexpv212121@gmail.com>
2015-06-10 15:16:47 +0300

    Add new route Categories

--- src/MySite/App_Start/RouteConfig.cs
+++ src/MySite/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MySite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Categories",
+                url: "categories/{id}",
+                defaults: new { controller = "Category", action = "ProductList", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
