fa81776cf1b6e93b277e04d6a6ae867e2deabe4d
Balian21 <alexpv212121@gmail.com>
2015-06-26 10:54:05 +0300

    Add Search on Site

---
+++ src/MySite/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Model;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySite.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        [HttpPost]
+        public ActionResult SearchProducts(string message)
+        {
+            //ViewBag.Message = message;
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var result = session.QueryOver<Product>()
+                                    .WhereRestrictionOn(p => p.Name).IsLike("%" + message + "%")
+                                    .List<Product>();
+
+                return View(result);
+            }
+        }
+    }
+}
