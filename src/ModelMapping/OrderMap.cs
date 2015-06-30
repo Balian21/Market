c4177b559be5e7798673110a140cbce13259ed89
Balian21 <alexpv212121@gmail.com>
2015-06-30 14:56:48 +0300

    add classes Cart, Order

---
+++ src/ModelMapping/OrderMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate;
+using FluentNHibernate.Mapping;
+using Model;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelMapping
+{
+    public class OrderMap : ClassMap<Order>
+    {
+        public OrderMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.TotalAmount);
+            HasMany(x => x.Products).KeyColumn("Product_id").Inverse().Cascade.AllDeleteOrphan();
+        }
+    }
+}
