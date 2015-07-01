19e9f817e2c3c5bee688fb2dd156136715242e90
Balian21 <alexpv212121@gmail.com>
2015-07-01 13:13:24 +0300

    Add Cart, CartLine in Model

---
+++ src/Model/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cart
+    {
+        private IList<CartLine> cartlines = new List<CartLine>();
+
+        public virtual int Id { get; set; }
+
+        public virtual IList<CartLine> CartLines { get; set; }
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = cartlines
+                .Where(p => p.Product.Id == product.Id)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                cartlines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+    }
+}
