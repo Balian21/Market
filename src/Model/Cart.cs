f7e3076628bc2b08ad7e8014f8088e548d2b2773
Balian21 <alexpv212121@gmail.com>
2015-07-02 12:27:12 +0300

    change add to cart

--- src/Model/Cart.cs
+++ src/Model/Cart.cs
@@ -9,10 +9,15 @@ namespace Model
     public class Cart
     {
         private IList<CartLine> cartlines = new List<CartLine>();
+        public CartLine cartline;
 
         public virtual int Id { get; set; }
 
-        public virtual IList<CartLine> CartLines { get; set; }
+        public virtual IList<CartLine> CartLines
+        {
+            get { return cartlines; }
+            set { cartlines = value; }
+        }
 
         public void AddItem(Product product, int quantity)
         {
