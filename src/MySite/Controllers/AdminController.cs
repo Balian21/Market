83f9e905fdc797deded424d5d612df1457ef6b1b
Balian21 <alexpv212121@gmail.com>
2015-07-06 16:01:56 +0300

    Add Admin Panel

---
+++ src/MySite/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ViewResult Index()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var products = session.QueryOver<Product>()
+                    .Where(p => p.Name != "").List();
+                return View(products);
+            }
+        }
+    }
+}
